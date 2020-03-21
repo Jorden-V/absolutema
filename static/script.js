9113c2679aa62d53d120b73ce9e0be144b71fddc
Jorden <jorden@jordens-mbp.home>
2020-03-21 15:43:20 +0000

    added meet the coaches scroller

--- static/script.js
+++ static/script.js
@@ -2,5 +2,23 @@ $( document ).ready(function(){
   $(".dropdown-button").dropdown();
   $(".button-collapse").sideNav();
   $('.carousel.carousel-slider').carousel({fullWidth: true});
+  $('#testimonial-carousel').owlCarousel({
+    autoplay: true,
+    dots: false,
+    nav: true,
+    loop:true,
+    navText: ['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:2
+        }
+    }
+})
 })
   
